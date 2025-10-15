8f78dda3684d5895a2c027790a2fca310c79777e
Dybenko Guajardo <dybenko1@yahoo.com.mx>
2025-10-15 13:54:49 +0800

    Finished the "rm" command, hence all bonus exercises are done.

--- cmd/rm.go
+++ cmd/rm.go
@@ -1,11 +1,13 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"strconv"
+	"task/db"
 
 	"github.com/spf13/cobra"
 )
@@ -13,28 +15,43 @@ import (
 // rmCmd represents the rm command
 var rmCmd = &cobra.Command{
 	Use:   "rm",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Deletes task, it will not be marked as completed.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("rm called")
+		// Converting input by user to int and creating list of task to be removed
+		var finishedTasks []int
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to parse the argument: ", arg)
+			} else {
+				finishedTasks = append(finishedTasks, id)
+			}
+		}
+
+		// Looping to all list task to remove the ones marked by the user
+		tasks, err := db.AllTasks(db.TaskBucket)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Looping through tasks marked as completed
+		for _, finishedTask := range finishedTasks {
+			if finishedTask <= 0 || finishedTask > len(tasks) {
+				fmt.Println("Invalid task number:", finishedTask)
+				continue
+			}
+			task := tasks[finishedTask-1]
+			// Deleting task from Pending task bucket
+			err = db.DeleteTask(task.Key)
+			if err != nil {
+				log.Fatal(err)
+			} else {
+				fmt.Printf("You have deleted the \"%s\" task.\n", task.Value)
+			}
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(rmCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// rmCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// rmCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
